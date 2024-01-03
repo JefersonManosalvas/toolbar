3668814f1398c2ebd2f71ffb1c68458047d272f5
JefersonManosalvas <132539368+JefersonManosalvas@users.noreply.github.com>
2024-01-03 10:36:40 -0500

    creacion del toolbar

---
+++ app/src/main/java/com/jpml/toolbar/MainActivity.java
@@ -0,0 +1,31 @@
+package com.jpml.toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView leftIcon = findViewById(R.id.left_icon);
+        ImageView rightIcon = findViewById(R.id.right_icon);
+        TextView title = findViewById(R.id.title);
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+}
